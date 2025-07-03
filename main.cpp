8853c931308c69dfc86db479fbe3ba7adf4697a0
Tote1514 <joseantoniocastrom5@gmail.com>
2025-07-03 16:20:36 -0300

    Colocando timer visual do jogo e motivos para acabar

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
+#include <sstream>
 
 #include "Area.h"
 #include "Pinguim.h"
@@ -14,7 +16,6 @@
 void init();
 void reshape(int w, int h);
 void specialKeyboard(int key, int x, int y);
-void keyboard(unsigned char key, int x, int y);
 void doFrame(int value);
 void atualizarTempoJogo(int value);
 
@@ -27,6 +28,10 @@ int INITIAL_WIDTH = 800;
 int INITIAL_HEIGHT = 600;
 int window_top, window_side, window_front, window_free;
 std::vector<int> window_ids;
+
+bool acabouOJogo = false;
+bool jogadorGanhou = false;
+std::string razaoDeTerAcabado = "";
 
 Pinguim pinguim(1, 0.0f, 0.0f);
 Filhote filhote(0.0f, 0.0f, 0.0f);
@@ -46,6 +51,15 @@ std::vector<Gelo> gelos =
 	Gelo(13.0f, alturaDoChao, -7.0f),
 	Gelo(15.0f, alturaDoChao, 10.0f)
 };
+
+std::string formatTime(int totalSeconds) {
+	if (totalSeconds < 0) totalSeconds = 0;
+	int minutes = totalSeconds / 60;
+	int seconds = totalSeconds % 60;
+	std::ostringstream oss;
+	oss << std::setw(2) << std::setfill('0') << minutes << ":" << std::setw(2) << std::setfill('0') << seconds;
+	return oss.str();
+}
 
 void gerarPosicoesAleatoriasPeixes()
 {
@@ -81,10 +95,58 @@ void timerPeixes(int value)
     glutTimerFunc(10000, timerPeixes, 0);
 }
 
+void drawPlacar() {
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, glutGet(GLUT_WINDOW_WIDTH), 0, glutGet(GLUT_WINDOW_HEIGHT));
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+	glDisable(GL_LIGHTING);
+
+	int w = glutGet(GLUT_WINDOW_WIDTH);
+	int h = glutGet(GLUT_WINDOW_HEIGHT);
+
+	if (acabouOJogo || jogadorGanhou) {
+		std::string endMsg = jogadorGanhou ? "VOCE GANHOU!" : "ACABOU O JOGO!!";
+		if (jogadorGanhou) 
+			glColor3f(0.1f, 0.8f, 0.1f);
+		else 
+			glColor3f(0.8f, 0.1f, 0.1f);
+
+
+		glRasterPos2i(w / 2 - 100, h / 2 + 20);
+		for (char c : endMsg) 
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+
+		if (acabouOJogo && !razaoDeTerAcabado.empty()) {
+			glColor3f(0.8f, 0.1f, 0.1f);
+			glRasterPos2i(w / 2 - 120, h / 2);
+			for (char c : razaoDeTerAcabado) 
+				glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+		}
+
+	}
+	else 
+	{
+		std::string sessionStr = "Jogo acaba em: " + formatTime(TEMPO_JOGO);
+		glColor3f(0.f, 0.f, 0.f);
+		glRasterPos2i(10, h - 40);
+		for (char c : sessionStr) 
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+	}
+
+	glEnable(GL_LIGHTING);
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+}
+
 void drawScene()
 {
 	// Plataforma de gelo
-
 	float zoom{ 40.f };
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, new float[4] {0.8f, 0.95f, 1.0f, 1.0f});
 	glMaterialfv(GL_FRONT, GL_SPECULAR, new float[4] {1.0f, 1.0f, 1.0f, 1.0f});
@@ -105,8 +167,6 @@ void drawScene()
 	for (auto& gelo : gelos)
 		gelo.desenha();
 
-
-	glutSwapBuffers();
 }
 
 void display_top()
@@ -119,6 +179,8 @@ void display_top()
 						pinguim.getX(), pinguim.getY(), pinguim.getZ(),
 						0, 0, -1);
 	drawScene();
+	drawPlacar();
+	glutSwapBuffers();
 }
 
 void display_side()
@@ -131,6 +193,8 @@ void display_side()
 						pinguim.getX(), pinguim.getY(), pinguim.getZ(),
 						0, 1, 0);
 	drawScene();
+	drawPlacar();
+	glutSwapBuffers();
 }
 
 void display_front()
@@ -142,6 +206,8 @@ void display_front()
 	gluLookAt(pinguim.getX() + 20.0f, pinguim.getY() + 5.0f, pinguim.getZ(), pinguim.getX(), pinguim.getY(), pinguim.getZ(),
 						0, 1, 0);
 	drawScene();
+	drawPlacar();
+	glutSwapBuffers();
 }
 
 void display_free()
@@ -154,6 +220,8 @@ void display_free()
 						pinguim.getX(), pinguim.getY(), pinguim.getZ(),
 						0, 1, 0);
 	drawScene();
+	drawPlacar();
+	glutSwapBuffers();
 }
 
 void reshape(int w, int h)
@@ -187,30 +255,6 @@ void specialKeyboard(int key, int x, int y)
 	glutPostRedisplay();
 }
 
-void keyboard(unsigned char key, int x, int y)
-{
-	switch (tolower(key))
-	{
-	case 'w':
-		pinguim.orientarPara(0.0f, 1.0f);
-		pinguim.mover(0, -0.2f);
-		break;
-	case 's':
-		pinguim.orientarPara(0.0f, -1.0f);
-		pinguim.mover(0, 0.2f);
-		break;
-	case 'a':
-		pinguim.orientarPara(-1.0f, 0.0f);
-		pinguim.mover(-0.2f, 0);
-		break;
-	case 'd':
-		pinguim.orientarPara(1.0f, 0.0f);
-		pinguim.mover(0.2f, 0);
-		break;
-	}
-	glutPostRedisplay();
-}
-
 void doFrame(int value)
 {
 	for (auto &peixe : cardume)
@@ -225,11 +269,8 @@ void doFrame(int value)
 
 		if (areaGelo.colideCom(pinguim.getArea()))
 		{
-			std::cout << "Pinguim escorregou no gelo!" << std::endl;
-			std::cout << "Jogo acabou!" << std::endl;
-			std::cout << "Voce perdeu!" << std::endl;
-
-			exit(0);
+			razaoDeTerAcabado = "O pinguim escorregou no gelo e caiu no mar!";
+			acabouOJogo = true;
 		}
 
 	}
@@ -253,15 +294,19 @@ void atualizarTempoJogo(int value)
 	if (filhote.verificarSeEstaVivo())
 	{
 		std::cout << "Filhote morreu!" << std::endl;
-		exit(0);
+		razaoDeTerAcabado = "O filhote morreu de fome.";
+		acabouOJogo = true;
 	}
 
-	if (TEMPO_JOGO > 0)
-		TEMPO_JOGO--;
-	else
+	if (!acabouOJogo || jogadorGanhou)
 	{
-		std::cout << "Fim de jogo!" << std::endl;
-		exit(0);
+		if (TEMPO_JOGO > 0)
+			TEMPO_JOGO--;
+		else
+		{
+			std::cout << "Fim de jogo!" << std::endl;
+			jogadorGanhou = true;
+		}
 	}
 
 	std::cout << "Tempo de jogo restante: " << TEMPO_JOGO << " segundos." << std::endl;
@@ -312,47 +357,39 @@ int main(int argc, char **argv)
 	const int baseX = 50;
 	const int baseY = 50;
 
-	// Free camera (canto superior esquerdo)
 	glutInitWindowSize(winW, winH);
 	glutInitWindowPosition(baseX + 0 * (winW + spacing), baseY + 0 * (winH + spacing));
 	window_free = glutCreateWindow("Free Camera");
 	init();
 	glutDisplayFunc(display_free);
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialKeyboard);
 	window_ids.push_back(window_free);
 
-	// Top view (canto superior direito)
 	glutInitWindowSize(winW, winH);
 	glutInitWindowPosition(baseX + 1 * (winW + spacing), baseY + 0 * (winH + spacing));
 	window_top = glutCreateWindow("Top Camera");
 	init();
 	glutDisplayFunc(display_top);
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialKeyboard);
 	window_ids.push_back(window_top);
 
-	// Side view (canto inferior esquerdo)
 	glutInitWindowSize(winW, winH);
 	glutInitWindowPosition(baseX + 0 * (winW + spacing), baseY + 1 * (winH + spacing));
 	window_side = glutCreateWindow("Side Camera");
 	init();
 	glutDisplayFunc(display_side);
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialKeyboard);
 	window_ids.push_back(window_side);
 
-	// Front view (canto inferior direito)
 	glutInitWindowSize(winW, winH);
 	glutInitWindowPosition(baseX + 1 * (winW + spacing), baseY + 1 * (winH + spacing));
 	window_front = glutCreateWindow("Front Camera");
 	init();
 	glutDisplayFunc(display_front);
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialKeyboard);
 	window_ids.push_back(window_front);
 
